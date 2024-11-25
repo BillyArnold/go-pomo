7deb8e881a70d6d04f017de465c9f20aaa7fec3f
Billy Arnold <billyarnoldd@gmail.com>
2024-11-25 21:25:46 +0000

    input string func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/billyarnold/go-pomo
+
+go 1.22.0
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Welcome to the pomodoro cli application")
+
+	inputVal, err := inputString("Enter your name")
+	if err != nil {
+		fmt.Println("there was an input error")
+	}
+
+	input, err := inputString("Enter another word")
+	if err != nil {
+		fmt.Println("there was an input error")
+	}
+
+	fmt.Println(inputVal, input)
+
+}
+
+func inputString(prompt string) (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%v \n", prompt)
+
+	val, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	val = strings.TrimSpace(val)
+
+	return val, nil
 }
